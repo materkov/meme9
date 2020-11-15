6c1ea230a0adde3ca0b3f076214f434cee5777bd
Maks Materkov <materkov@gmail.com>
2020-11-15 12:22:45 +0300

    Linter fix

--- api/handlers/common.go
+++ api/handlers/common.go
@@ -1,26 +1,3 @@
 package handlers
 
-import (
-	"log"
-	"net/http"
-
-	"github.com/gogo/protobuf/jsonpb"
-	"github.com/gogo/protobuf/proto"
-	"github.com/materkov/meme9/api/pb"
-)
-
-func writeResponse(w http.ResponseWriter, resp proto.Message) {
-	m := jsonpb.Marshaler{}
-	_ = m.Marshal(w, resp)
-}
-
-func writeError(w http.ResponseWriter, err *pb.ErrorRenderer) {
-	writeResponse(w, err)
-}
-
-func writeInternalError(w http.ResponseWriter, err error) {
-	log.Printf("[ERROR] Internal error: %s", err)
-	writeError(w, &pb.ErrorRenderer{DisplayText: "Internal error"})
-}
-
 const VKAppID = 7260220
