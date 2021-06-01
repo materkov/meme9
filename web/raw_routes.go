a3bdce4f20279143637528b10f1d5927b1a10067
Maks Materkov <materkov@gmail.com>
2021-06-01 18:04:45 +0300

    Add meta header

--- web/raw_routes.go
+++ web/raw_routes.go
@@ -149,8 +149,9 @@ func handleDefault(w http.ResponseWriter, r *http.Request) {
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-    <meta charset="utf-8">
     <title>meme</title>
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 <script>
