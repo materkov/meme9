aa52fcdeb0a23a5ba7d5efd58cd13a8a2dd8f41b
Maks Materkov <m.materkov@corp.vk.com>
2021-08-14 21:18:37 +0300

    Sql

--- web2/migrations/01_init.sql
+++ web2/migrations/01_init.sql
@@ -2,11 +2,15 @@ create table post
 (
     id      int auto_increment primary key,
     text    text,
-    user_id int  not null default 0
+    user_id int not null default 0
 );
 
 create table user
 (
-    id      int auto_increment primary key,
-    name    varchar(200) not null default ''
+    id    int auto_increment primary key,
+    name  varchar(200) not null default '',
+    vk_id int,
+
+    constraint user_vk_id_uindex
+        unique (vk_id)
 );
