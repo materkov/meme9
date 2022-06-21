8a19d70f63c010fb78de5b0bf6e30cd6ed164374
Maks Materkov <m.materkov@corp.vk.com>
2022-06-21 22:06:30 +0300

    Fix build script

---
+++ front4/build-prod.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const cssModulesPlugin = require("esbuild-css-modules-plugin");
+
+require("esbuild")
+    .build({
+        logLevel: "debug",
+        entryPoints: ["src/index.tsx"],
+        bundle: true,
+        outfile: "dist/bundle.js",
+        plugins: [cssModulesPlugin()],
+    })
+    .catch(() => process.exit(1));
