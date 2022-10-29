852a3ad69a17f90e1a7879128788052b9e8b4bba
m.materkov <m.materkov@corp.vk.com>
2022-10-29 19:39:21 +0500

    Fix onlineManager

--- front5/src/store/onlineManager.ts
+++ front5/src/store/onlineManager.ts
@@ -1,14 +1,26 @@
 import {api} from "./types";
 
+const PING_INTERVAL = 179 * 60 * 1000; // 2 min 59 sec
+
 export function setOnline() {
+    localStorage.setItem('onlineManagerLastPing', Math.floor(Date.now()).toString())
+
     if (localStorage.getItem('authToken')) {
-        api("/setOnline", {});
+        api('/setOnline', {});
     }
 }
 
 export function runForever() {
+    const lastPing = parseInt(localStorage.getItem('onlineManagerLastPing') || '0');
+    const currentTs = Math.floor(Date.now());
+
+    let nextTime = PING_INTERVAL - (currentTs - lastPing);
+    if (nextTime < 1000) {
+        nextTime = 1000;
+    }
+
     setTimeout(() => {
         setOnline();
-        setInterval(setOnline, 2.9 * 60 * 1000);
-    }, 500);
+        setInterval(setOnline, PING_INTERVAL);
+    }, nextTime);
 }
