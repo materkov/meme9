b9f9e2da8a45a783514bc61b3dff9591c2aeea66
m.materkov <m.materkov@corp.vk.com>
2023-09-03 11:29:37 +0400

    CI fixes

---
+++ migrations/01_init.sql
@@ -0,0 +1,23 @@
+create database meme9;
+use meme9;
+
+create table edges
+(
+    id         int auto_increment
+        primary key,
+    from_id    int          not null,
+    to_id      int          not null,
+    edge_type  int          not null,
+    unique_key varchar(400) null,
+    constraint unique_edges_key
+        unique (from_id, edge_type, to_id, unique_key)
+);
+
+create table objects
+(
+    id       bigint auto_increment
+        primary key,
+    obj_type smallint not null,
+    data     json     not null
+);
+
